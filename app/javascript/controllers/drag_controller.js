fb4c2ce64975dc434cb65ebe347bd694de159633
Paul Whitehead <prwhitehead@gmail.com>
2020-08-19 22:11:10 +0100

    Remove position because it all works

---
+++ app/javascript/controllers/drag_controller.js
@@ -0,0 +1,21 @@
+import ApplicationController from './application_controller'
+import Sortable from "sortablejs"
+
+export default class extends ApplicationController {
+  connect() {
+    this.sortable = Sortable.create(this.element, {
+      onEnd: this.end.bind(this)
+    })
+  }
+
+  end(event) {
+    let id = event.item.dataset.id
+    let data = new FormData()
+    data.append("position", event.newIndex + 1)
+
+    fetch(this.data.get("url").replace(":id", id), {
+      method: "PATCH",
+      body: data
+    })
+  }
+}
